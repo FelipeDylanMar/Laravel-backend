eda1dd0277aff67f2a7517db49f32c943f05485a
unknown <felipe.back98@gmail.com>
2025-08-28 21:10:03 -0400

    feat: add TypeScript configuration files (tsconfig.json, vite.config.ts)

---
+++ frontend/vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+// @ts-ignore
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      }
+    }),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@': fileURLToPath(new URL('./src', import.meta.url ))
+    },
+  },
+  esbuild: {
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'pinia']
+  }
+})
